44072ed4f26af4a8fba238da04044e4b1cf03523
Mach1498 <Mach1498@users.noreply.github.com>
2017-07-08 19:06:16 -0400

    Create MVBLN.java

---
+++ MVBLN.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MVBLN {
+
+	private static int n = 9;           // Size of array
+	private static int k = 2;           // Allowed number of adjacency of 1
+	
+	public static void main(String[] args) {
+
+		int [] arr = generateRandomArray();
+
+		System.out.println("Given array: ");
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		System.out.println();
+
+		int[][] M = new int[n][k+1];
+		M[0][0] = arr[0];
+		M[1][0] = Math.max(arr[0], arr[1]);
+		
+		for (int i = 2; i < n; i++) {
+			M[i][0] = Math.max(M[i - 1][0], M[i - 2][0] + arr[i]);
+		}
+
+		for (int j = 1; j <= k; j++) {
+			M[0][j] = arr[0];
+			M[1][j] = arr[0] + arr[1];
+			
+			for (int i = 2; i < n; i++) {
+				M[i][j] = Math.max(M[i - 1][j], Math.max(M[i - 2][j] + arr[i], M[i - 1][j - 1] + arr[i]));
+			}
+		}
+		
+		for (int j = 0; j <= k; j++) {
+			System.out.print(j + ":  ");
+			for (int i = 0; i < n; i++) {
+				System.out.print(M[i][j] + "  ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	private static int[] generateRandomArray() {
+
+		ArrayList<Integer> list = new ArrayList<Integer>(n);
+		Random random = new Random();
+
+		for (int i = 0; i < n; i++) {
+			list.add(random.nextInt(100));
+		}
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = list.get(i);
+		}
+
+		return arr;
+	}
+
+}
+
+/*
+ * b[i]=0 => S=M[i-1, j] // a[i] not added to sum, b[1..i-1] has same number of
+ * adjacent 1s (j) as b[1..i] because b[i] is zero
+ * 
+ * b[i]=1 and b[i-1]=0 => S=M[i-2, j]+a[i] // a[i] added to sum, b[1..i-2] has
+ * same number of adjacencent 1s, because b[i-1] is zero
+ * 
+ * b[i]=1 and b[i-1]=1 => S=M[i-1, j-1]+a[i] // a[i] added to sum, since b[i]
+ * and b[i-1] are both 1, they count as adjacent 1s, so b[1..i-1] contains only
+ * j-1 adjacent 1s
+ */
